8b1db1cadb5e778f200b5fd70215d5cea0e33a0d
Sergiu Gherasim <sergeras4@gmail.com>
2025-09-12 21:17:01 +0300

    feat: improve register page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -7,6 +7,7 @@ import {
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
 import Dither from '@/components/Dither'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
   return (
@@ -16,28 +17,39 @@ const Register = () => {
           <Dither mouseRadius={0.2} colorNum={5} />
         </div>
 
-        <div className="xl:w-[40%] w-full flex flex-col gap-4 p-6 justify-center items-center">
-          <h1 className="text-3xl font-semibold">Create an account</h1>
+        <div className="xl:w-[40%] w-full flex flex-col p-6 justify-center items-center">
+          <div className="flex flex-col gap-1 items-center mb-4">
+            <h1 className="text-3xl font-semibold">Create an account</h1>
+            <p className="leading-7 opacity-55 max-w-xl">
+              Fill in all the data to register
+            </p>
+          </div>
 
           <div className="w-[80%] flex flex-col gap-2">
             <div className="flex gap-2">
               <Input placeholder="First Name" />
               <Input placeholder="Last Name" />
             </div>
+            <Input placeholder="IDNP" type="number" />
+            <Input placeholder="IDNO" type="number" />
+            <Input placeholder="Phone number" type="number" />
+            <Input placeholder="Email" type="email" />
 
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button className="w-full font-normal text-neutral-600" variant="outline">
-                  Select Legal Form of the Farm
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-full">
-                <DropdownMenuItem>Household farm</DropdownMenuItem>
-                <DropdownMenuItem>LLC</DropdownMenuItem>
-                <DropdownMenuItem>Cooperative</DropdownMenuItem>
-                <DropdownMenuItem>Individual enterprise</DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+            <div className="w-full mt-2">
+              <Button className="w-full">Register</Button>
+              <div className="w-full flex justify-between">
+                <Link to="#">
+                  <small className="text-sm leading-none font-medium opacity-50">
+                    Need help
+                  </small>
+                </Link>
+                <Link to="/login">
+                  <small className="text-sm leading-none font-medium opacity-50">
+                    Already have an account
+                  </small>
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
