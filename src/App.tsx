b421c712cdf81cb19e0761d2d8a76857b5ebbabb
Sergiu Gherasim <sergeras4@gmail.com>
2025-09-12 17:05:40 +0300

    feat: add supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+)
+
+console.log(supabase)
 
 const App = () => {
   return (
