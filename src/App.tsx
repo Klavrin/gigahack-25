bb4023e42b132961b75ca043049ce8bf511fd4a8
Sergiu Gherasim <sergeras4@gmail.com>
2025-09-12 17:25:45 +0300

    feat: add /register page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import { createClient } from '@supabase/supabase-js'
+import Register from './pages/Register'
 
 const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
 )
 
-console.log(supabase)
-
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   )
