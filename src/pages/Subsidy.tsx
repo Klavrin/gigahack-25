34fbae15c6d4c2272d5d05274112eaee32c72f5b
Sergiu Gherasim <sergeras4@gmail.com>
2025-09-13 20:41:07 +0300

    feat: add stars to subsidies

--- src/pages/Subsidy.tsx
+++ src/pages/Subsidy.tsx
@@ -1,12 +1,13 @@
 import Layout from '@/components/Layout'
+import { Star } from 'lucide-react'
 
 const typeSubsidies = [
-  'Land Subsidy',
-  'Tax Incentives',
-  'Grants',
-  'Low-Interest Loans',
-  'Training Programs',
-  'Infrastructure Support'
+  { name: 'Land Subsidy', starred: true },
+  { name: 'Tax Incentives', starred: false },
+  { name: 'Grants', starred: false },
+  { name: 'Low-Interest Loans', starred: false },
+  { name: 'Training Programs', starred: false },
+  { name: 'Infrastructure Support', starred: false }
 ]
 
 const Subsidy = () => {
@@ -16,12 +17,15 @@ const Subsidy = () => {
       <div>
         {typeSubsidies.map((item) => (
           <div
-            key={item}
+            key={item.name}
             className="p-4 mt-4 border border-border rounded-lg hover:shadow-lg transition-shadow cursor-pointer"
           >
-            <div className="text-2xl font-semibold">{item}</div>
+            <div className="flex gap-2 items-center">
+              {item.starred && <Star color="#fcba03" fill="#fcba03" />}
+              <div className="text-2xl font-semibold">{item.name}</div>
+            </div>
             <div className="mt-2 text-md md:text-sm text-muted-foreground">
-              Detailed information about {item} including eligibility criteria,
+              Detailed information about {item.name} including eligibility criteria,
               application process, and benefits.
             </div>
           </div>
